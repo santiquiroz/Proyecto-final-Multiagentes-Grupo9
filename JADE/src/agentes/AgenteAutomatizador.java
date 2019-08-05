e46b9bba06b8491dbd15052b05418327883ec0de
Santiago Quiroz upegui <squirozu@unal.edu.co>
2019-08-05 11:59:27 -0500

    ESQUELETO
    AUTOMATIZADOR, IDENTIFICADOR

---
+++ JADE/src/agentes/AgenteAutomatizador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agentes;
+
+import jade.core.Agent;
+
+/**
+ *
+ * @author santi
+ */
+public class AgenteAutomatizador extends Agent{
+    protected void setup() {
+        
+    }
+}
