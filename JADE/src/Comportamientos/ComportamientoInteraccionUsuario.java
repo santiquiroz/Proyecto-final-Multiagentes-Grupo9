6f086564a34c7fc23aedddf689655c87a6d32a67
Crisbo <cdramirezh@unal.edu.co>
2019-08-05 17:55:01 -0500

    PlantillaAgenteInteraccionUsuario
    Se meten las plantillas del agente interacción usuario y su comportamineto ComportamientoInteraccionUsuario.java

---
+++ JADE/src/Comportamientos/ComportamientoInteraccionUsuario.java
@@ -0,0 +1,19 @@
+package Comportamientos;
+
+import jade.core.behaviours.SimpleBehaviour;
+
+public class ComportamientoInteraccionUsuario extends SimpleBehaviour{
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean done() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- JADE/src/agentes/AgenteInteraccionUsuario.java
+++ JADE/src/agentes/AgenteInteraccionUsuario.java
@@ -1,15 +1,34 @@
-/**
- *
- * @author David Ospina
- */
-
 package agentes;
 
+import Comportamientos.*;
+import jade.content.lang.Codec;
+import jade.content.lang.sl.SLCodec;
+import jade.content.onto.Ontology;
 import jade.core.Agent;
+import ontologias.InteraccionUsuarioOntology;
 
 public class AgenteInteraccionUsuario extends Agent{
     
+    public Codec codec = new SLCodec();
+    public Ontology ontologia = InteraccionUsuarioOntology.getInstance();
+    
     protected void setup(){
+        
+        /**
+         * Esta linea se utiliza cuando se envian argumentos desde
+         * la declaracion de los argumentos
+        
+        String argumentos;
+        Object[] args = getArguments();
+        argumentos = args[0].toString();
+        if (argumentos.equals("terminar agente")){            
+            System.out.println("terminando agente");   
+            doDelete();
+        }
+        
+        */
+        
+        addBehaviour(new ComportamientoInteraccionUsuario());
         
     }
 }
