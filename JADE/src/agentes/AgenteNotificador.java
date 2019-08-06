d3d3d1996f3f1589b22f59b29dacee73ca8e14f6
Santiago Quiroz upegui <squirozu@unal.edu.co>
2019-08-05 23:07:03 -0500

    base de datos
    manejo de consultas y transacciones basicas programadas

--- JADE/src/agentes/AgenteNotificador.java
+++ JADE/src/agentes/AgenteNotificador.java
@@ -10,6 +10,10 @@ import jade.content.lang.Codec;
 import jade.content.lang.sl.SLCodec;
 import jade.content.onto.Ontology;
 import jade.core.Agent;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import ontologias.InteraccionUsuarioOntology;
 import system.DataBase.Conexion;
 
@@ -21,24 +25,21 @@ public class AgenteNotificador extends Agent{
     @Override
     protected void setup(){
         
-        Conexion conn = new Conexion();
+          /**
+             * Esta linea se utiliza cuando se envian parametros desde
+             * la declaracion de los argumentos
+             * 
+             * String argumentos;
+             * Object[] args = getArguments();
+             * argumentos = args[0].toString();
+             * if (argumentos.equals("terminar agente")){
+             * System.out.println("terminando agente");
+             * doDelete();
+             * }
+             * 
+             */
         
-        System.out.println(conn.getConection());
-        
-        conn.desconectar();
-        /**
-         * Esta linea se utiliza cuando se envian parametros desde
-         * la declaracion de los argumentos
-        
-        String argumentos;
-        Object[] args = getArguments();
-        argumentos = args[0].toString();
-        if (argumentos.equals("terminar agente")){            
-            System.out.println("terminando agente");   
-            doDelete();
-        }
-        
-        */
+
         
         addBehaviour(new ComportamientoNotificador(this, 0));
         
