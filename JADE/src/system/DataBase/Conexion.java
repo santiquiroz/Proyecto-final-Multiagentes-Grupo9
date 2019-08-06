f3d4b6c45baf8c60d03b7d138a2c05b4766e2e30
Santiago Quiroz upegui <squirozu@unal.edu.co>
2019-08-05 20:37:53 -0500

    Create conexion
    Creando algoritmo para conectar a la base de datos en mysql

---
+++ JADE/src/system/DataBase/Conexion.java
@@ -0,0 +1,50 @@
+/*
+Clase que sirve para realizar una conexion a una base de datos mysql 
+con la direccion, usuario y contraseña establecidos en el constructor.
+ */
+package system.DataBase;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author santi
+ */
+public class Conexion {
+    private static Connection conn;
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private String user = "root";
+    private String password = "";
+    private String url = "jdbc:mysql://localhost:3306/datosusuario";
+    
+    public Conexion (){
+        conn = null;
+        try{
+            Class.forName(driver);
+            conn = (Connection) DriverManager.getConnection(url,user,password);
+            if(conn!= null){
+                System.out.println("conexion establecida!");
+            }
+        }catch (ClassNotFoundException | SQLException e){
+            System.out.println("conexion fallida: "+e);
+        }
+    }
+    
+    /*
+    metodo para obtener la conexion activa a la base de datos.
+    */
+    public Connection getConection(){
+        return conn;
+    }
+    /*
+    metodo para terminar la conexion a la base de datos activa.
+    */
+    public void desconectar(){
+        conn = null;
+        if (conn == null) {
+            System.out.println("conexion terminada con exito !");
+        }
+    }
+}
