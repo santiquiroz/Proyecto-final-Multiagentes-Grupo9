2830258760765b515b82e707f7ec9458715d64c1
Santiago Quiroz upegui <squirozu@unal.edu.co>
2019-08-07 21:29:15 -0500

    documentacion terminada

--- Actores/src/system/DataBase/DataBase.java
+++ Actores/src/system/DataBase/DataBase.java
@@ -22,6 +22,7 @@ public class DataBase {
     private ArrayList nombresColumnas;
     private int numColumnas;
     private ResultSet consulta;
+    
     public DataBase() {
         try {
             conn = new Conexion();
@@ -49,7 +50,14 @@ public class DataBase {
         resultado=new ArrayList();
         nombresColumnas = new ArrayList();
     }
-    
+    /**
+     * Este metodo realiza una consulta en la base de datos y devuelve un
+     * arraylist de arraylist que representan el resultado de la consulta [tabla].
+     * 
+     *
+     * @param SQL Codigo SQLConsulta para nuestra consulta
+     * @return Tabla de la consulta
+     */
     public ArrayList select(String SQL){
         try {
              st = conn.getConection().prepareStatement(SQL);
@@ -78,7 +86,12 @@ public class DataBase {
         }
         return resultado;
     }
-    
+    /**
+     * Este metodo realiza una insercion en la base de datos por medio de una actualizacion.
+     * 
+     *
+     * @param SQL Codigo SQLConsulta para nuestra insercion.
+     */
     public void insert(String SQL){
         try {
              st = conn.getConection().prepareStatement(SQL);
@@ -87,6 +100,13 @@ public class DataBase {
             Logger.getLogger(DataBase.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    /**
+     * Este metodo realiza una actualizacion en la base de datos por medio de una actualizacion.
+     * 
+     *
+     * @param SQL Codigo SQLConsulta para nuestra actualizacion.
+     * @return entero que informa si la actualizacion fue correcta o no.
+     */
     public int update(String SQL){
         int resultadoActualizacion=-1;
         try {
@@ -97,6 +117,13 @@ public class DataBase {
         }
         return resultadoActualizacion;
     }
+    /**
+     * Este metodo realiza una eliminacion en la base de datos por medio de una actualizacion.
+     * 
+     *
+     * @param SQL Codigo SQLConsulta para nuestra insercion.
+     * @return entero que informa si la actualizacion fue correcta o no.
+     */
     public int delete(String SQL){
         int resultadoActualizacion=-1;
         try {
@@ -108,21 +135,48 @@ public class DataBase {
         return resultadoActualizacion;
     }
     
-    
+    /**
+     * Este metodo retorna el valor en cadena del elemento en una posicion especificada del resultado .
+     * 
+     *
+     * @param fila fila del valor deseado en la matriz del resultado de consulta.
+     * @param columna columna del valor deseado en la matriz del resultado de consulta.
+     * @return valor string del elemento en la posicion especificada.
+     */
     public String getValueOn(int fila, int columna){
         return String.valueOf(((ArrayList)resultado.get(columna)).get(fila));
     }
-    
+    /**
+     * Este metodo busca obtiene un arraylist con los nombres en orden de cada columna.
+     * 
+     * @return arraylist con los nombres en orden de cada columna.
+     */
     public ArrayList getColumnNames(){
         return nombresColumnas;
     }
-    public ArrayList getLastResult(){
-        return resultado;
-    }
-    public ResultSet getLastResultSet(){
-        return consulta;
-    }
+    /**
+     * Este metodo obtiene el numero de columnas en la ultima consulta.
+     * 
+     * @return entero que representa la cantidad de columnas de la ultima .
+     */
     public int getColumnNum(){
         return numColumnas;
     }
+    /**
+     * Este metodo obtiene el arraylist de arraylist que representa la ultima consulta realizada.
+     * 
+     * @return arraylist de arraylist..
+     */
+    public ArrayList getLastResult(){
+        return resultado;
+    }
+    /**
+     * Este metodo obtiene el ResultSet que representa la ultima consulta realizada.
+     * 
+     * @return ResultSet srolleable que representa la ultima consulta..
+     */
+    public ResultSet getLastResultSet(){
+        return consulta;
+    }
+    
 }
