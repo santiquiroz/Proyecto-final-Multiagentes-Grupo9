a0a25a5e501caf0513ed117bddea1e3f092425e4
Santiago Quiroz upegui <squirozu@unal.edu.co>
2019-08-09 10:50:54 -0500

    codigo de mensaje

--- JADE/src/agentes/AgenteAutomatizador.java
+++ JADE/src/agentes/AgenteAutomatizador.java
@@ -2,6 +2,7 @@ package agentes;
 
 import jade.core.Agent;
 import Comportamientos.*;
+import ontologias.Notificacion;
 
 /**
  *
@@ -12,6 +13,8 @@ public class AgenteAutomatizador extends Agent{
     @Override
     protected void setup() {
         addBehaviour(new ComportamientoAutomatizador());
+        Notificacion notificacion = new Notificacion();
+        notificacion.notificar("squirozu@unal.edu.co,dospinao@unal.edu.co,cdramirezh@unal.edu.co", "CORREO DE PRUEBA.. puto el que lo lea", "Este es un puto correo de prueba.");
     }
 }
     
