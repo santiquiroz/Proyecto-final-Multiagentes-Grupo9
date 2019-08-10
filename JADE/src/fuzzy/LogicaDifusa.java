3f7f0e6157ba443e22e51d373dbe9e6d41983dc0
David18236 <39032388+David18236@users.noreply.github.com>
2019-08-10 14:34:34 -0500

    Se agrega modulo fuzzyficador

---
+++ JADE/src/fuzzy/LogicaDifusa.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fuzzy;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.Rule;
+
+/**
+ * Test parsing an FCL file
+ * @author pcingola@users.sourceforge.net
+ */
+public class LogicaDifusa {
+
+    public static void iluminar(int luminocidad, int presencia) throws Exception {
+        // Load from 'FCL' file
+        String fileName = "src/logica/difusa/Archivo FCL.fcl";
+        FIS fis = FIS.load(fileName, true);
+        // Error while loading?
+        if (fis == null) {
+            System.err.println("Can't load file: '" + fileName + "'");
+            return;
+        }
+        // Set inputs
+        fis.setVariable("luminocidad", luminocidad);
+        fis.setVariable("presencia", presencia);
+
+        // Evaluate
+        fis.evaluate();
+
+        // Show
+        JFuzzyChart.get().chart(fis.getFunctionBlock("fuzzy"));
+
+        Double x = fis.getVariable("activacion").getLatestDefuzzifiedValue();
+        System.err.println("Para los valores de salida el grado de pertenencia es: " + x);
+        
+        // Show rules
+        for (Rule r : fis.getFunctionBlock("fuzzy").getFuzzyRuleBlock("No1").getRules()) {
+            System.out.println(r);
+        }
+    }
+}
